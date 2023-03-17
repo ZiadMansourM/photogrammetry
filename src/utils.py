d42885c76904c39561d568381a9be9bf44ce5d55
Ziad Mansour <ziadmansour.4.9.2000@gmail.com>
2023-03-17 21:19:34 +0200

    ✅ DONE: first draft

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,20 @@
+import time
+import os
+
 import cv2 as OpenCV
 from matplotlib import pyplot as plt
-import os
+
+def timeit_utils(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        with open("log.txt", "a") as f:
+            f.write(
+                f"Function '{func.__name__}' took {end_time - start_time} seconds to execute.\n"
+            )
+        return result
+    return wrapper
 
 def display_images(image, title = None) -> None:
     if image.ndim == 2:
@@ -13,9 +27,11 @@ def display_images(image, title = None) -> None:
 def read_images_rbg(imagePath: str):
     return OpenCV.cvtColor(OpenCV.imread(imagePath), OpenCV.COLOR_BGR2RGB)
 
+@timeit_utils
 def rgp_to_gray(images):
     return [OpenCV.cvtColor(image, OpenCV.COLOR_RGB2GRAY) for image in images]
 
+@timeit_utils
 def read_images(folderPath):
     files = sorted(os.listdir(folderPath))
     return [
