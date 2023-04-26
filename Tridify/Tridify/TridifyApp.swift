e74912c038258a23c22c38d5a212877dcc402303
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-04-26 20:34:53 +0200

    Add app files

---
+++ Tridify/Tridify/TridifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TridifyApp.swift
+//  Tridify
+//
+//  Created by Maged Alosali on 26/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TridifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
