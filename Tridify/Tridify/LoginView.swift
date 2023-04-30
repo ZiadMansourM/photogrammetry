efe444e0e15562f85349d78012d9a0db4d2422f0
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-04-30 23:45:47 +0300

    create login view

---
+++ Tridify/Tridify/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  Tridify
+//
+//  Created by Maged Alosali on 28/04/2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .foregroundColor(.red)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
