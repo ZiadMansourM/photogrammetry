e1939aa39a0757ac885d81509c88ba0457390233
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-05-14 15:37:49 +0300

    Create tab view
    and organize the files

---
+++ Tridify/Tridify/Screens/CameraView.swift
@@ -0,0 +1,20 @@
+//
+//  CameraView.swift
+//  Tridify
+//
+//  Created by Maged Alosali on 09/05/2023.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CameraView_Previews: PreviewProvider {
+    static var previews: some View {
+        CameraView()
+    }
+}
---
+++ Tridify/Tridify/Screens/HomeView.swift
@@ -0,0 +1,37 @@
+//
+//  HomeView.swift
+//  Tridify
+//
+//  Created by Maged Alosali on 06/05/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @AppStorage("onBoarding") var onBoarding = false
+    
+    var body: some View {
+        TabView {
+            Text("First View")
+                .tabItem {
+                    Image(systemName: "scale.3d")
+                    Text("Captures")
+                }
+            CameraView()
+                .tabItem {
+                    Image(systemName: "camera.fill")
+                    Text("Camera")
+                }
+            
+        }
+
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+        HomeView().preferredColorScheme(.dark)
+    }
+}
