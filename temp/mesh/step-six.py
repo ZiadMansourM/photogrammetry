eab87bffa0e51c9fea8268354c5c5d50d1f79224
MohameWZS <mohamedwael.31.8.2000@gmail.com>
2023-05-20 22:34:39 +0300

    FIX: Refactored Pipeline

--- temp/mesh/step-six.py
+++ temp/mesh/step-six.py
@@ -1,5 +1,6 @@
 import numpy as np
 import open3d as o3d
+import matplotlib.pyplot as plt
 
 # Define the number of points in the point cloud
 N = 1_000_000
@@ -17,48 +18,27 @@ z *= 1.0
 # Combine the coordinates into a single NumPy array
 point_cloud = np.column_stack((x, y, z))
 
-# Print max and min values of each axis:
-print(f"X: {point_cloud[:,0].min()} to {point_cloud[:,0].max()}")
-print(f"Y: {point_cloud[:,1].min()} to {point_cloud[:,1].max()}")
-print(f"Z: {point_cloud[:,2].min()} to {point_cloud[:,2].max()}")
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-# Generate point cloud
-# point_cloud = np.random.rand(10000, 3)
-
-# Define scaling factor
-scale_factor = 3.0
-
-# Scale the point cloud
-scaled_point_cloud = point_cloud * scale_factor
-
-print(f"X: {scaled_point_cloud[:,0].min()} to {scaled_point_cloud[:,0].max()}")
-print(f"Y: {scaled_point_cloud[:,1].min()} to {scaled_point_cloud[:,1].max()}")
-print(f"Z: {scaled_point_cloud[:,2].min()} to {scaled_point_cloud[:,2].max()}")
-
-# Plot histogram of points_cloud
-plt.hist(point_cloud.flatten(), bins=50, alpha=0.5, label='Before Scaling')
-# Plot histogram of scaled_point_cloud
-plt.hist(scaled_point_cloud.flatten(), bins=50, alpha=0.5, label='After Scaling')
-
-# Add titles and labels to the plot
-plt.title('Point Cloud Histogram')
-plt.xlabel('Point Value')
-plt.ylabel('Frequency')
-plt.legend(loc='upper right')
-
-# Show the plot
-plt.show()
-
-# Save the point cloud to a file
-# np.savetxt('point_cloud.txt', point_cloud)
-
+# Create an Open3D PointCloud object
 pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(point_cloud[:,:3])
 
+# # Save it as a.STL file
+o3d.io.write_point_cloud("cube_point_cloud.ply", pcd)
+
+# Visualize the point cloud
 o3d.visualization.draw_geometries([pcd])
 
-# Save it as a.STL file
-o3d.io.write_point_cloud("point_cloud.ply", pcd)
+# Estimate the normals for the point cloud
+pcd.estimate_normals()
+
+# Apply the Ball-Pivoting Algorithm to create a mesh
+radii = [0.005, 0.01, 0.02, 0.04]
+bpa_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(
+    pcd, o3d.utility.DoubleVector(radii)
+)
+
+# Save the mesh as an STL file
+o3d.io.write_triangle_mesh("cube_point_cloud.stl", bpa_mesh)
+
+# Visualize the point cloud
+o3d.visualization.draw_geometries([bpa_mesh])
