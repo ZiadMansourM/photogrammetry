6907450d1d997e99d3cdff07b3a4f86de8bafbd9
Ziad Mansour <ziadmansour.4.9.2000@gmail.com>
2023-05-30 04:50:09 +0300

    🔧 FIX: path

--- scripts/rename.py
+++ scripts/rename.py
@@ -8,7 +8,10 @@ def rename_images(directory: str) -> None:
     - directory: The path to the directory containing the images.
     """
     # Get a list of all .JPG files in the directory
-    image_files = glob.glob(os.path.join(directory, "*.JPG.mask.png"))
+    image_files = glob.glob(os.path.join(directory, "*.jpg.mask.png"))
+    # sort images by _number.jpg
+    image_files.sort(key=lambda x: int(x.split("/")[-1].split(".")[0].split("_")[1]))
+    print(f"Found {len(image_files)} images in {directory}")
 
     # Iterate over the image files and rename them
     for idx, image_file in enumerate(image_files, start=1):
@@ -16,11 +19,11 @@ def rename_images(directory: str) -> None:
         new_file_name = f"{idx}.jpg"
         new_file_path = os.path.join(directory, new_file_name)
 
-        # Rename the file
+        # # Rename the file
         os.rename(image_file, new_file_path)
 
         print(f"Renamed {image_file} to {new_file_path}")
 
 # Replace 'path_to_directory' with the actual path to the directory containing the images
-path_to_directory = r"C:\Users\yousf\OneDrive\Desktop\University\Graduation Project\Codes\photogrammetry\src\data\hammer\masked_images"
+path_to_directory = "src/data/cottage/masks"
 rename_images(path_to_directory)
