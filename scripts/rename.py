e407125c65fbfccba29df8e62396baab907ab6f7
MohameWZS <mohamedwael.31.8.2000@gmail.com>
2023-06-01 06:32:55 +0300

    FIX: File Conversion

--- scripts/rename.py
+++ scripts/rename.py
@@ -1,5 +1,7 @@
 import os
 import glob
+from PIL import Image
+
 
 def rename_images(directory: str) -> None:
     """Renames all .JPG images in a directory to a range of numbers and changes their extension to .jpg.
@@ -8,22 +10,21 @@ def rename_images(directory: str) -> None:
     - directory: The path to the directory containing the images.
     """
     # Get a list of all .JPG files in the directory
-    image_files = glob.glob(os.path.join(directory, "*.jpg.mask.png"))
+    image_files = glob.glob(os.path.join(directory, "*.png"))
     # sort images by _number.jpg
-    image_files.sort(key=lambda x: int(x.split("/")[-1].split(".")[0].split("_")[1]))
+    # image_files.sort(key=lambda x: int(x.split("\\")[-1].split(".")[0].split("_")[1]))
+    image_files.sort(key=lambda x: int(x.split("\\")[-1].split(".")[0]))
     print(f"Found {len(image_files)} images in {directory}")
 
     # Iterate over the image files and rename them
     for idx, image_file in enumerate(image_files, start=1):
-        # Create the new file name with the updated extension
-        new_file_name = f"{idx}.jpg"
-        new_file_path = os.path.join(directory, new_file_name)
+        im = Image.open(image_file)
 
-        # # Rename the file
-        os.rename(image_file, new_file_path)
+        rgb_im = im.convert('RGB')
+        rgb_im.save(f"{directory}/{idx}.jpg")
 
-        print(f"Renamed {image_file} to {new_file_path}")
+        print(f"Renamed {image_file} to {directory}/{idx}.jpg")
 
 # Replace 'path_to_directory' with the actual path to the directory containing the images
-path_to_directory = "src/data/cottage/masks"
+path_to_directory = "src/data/fountain/images"
 rename_images(path_to_directory)
