6fdd2df78a9d696a34fa270639b7076237e5023e
YousefGaafer <yousfjaafer@gmail.com>
2023-06-11 03:57:24 +0300

    🔧 FIX: Tuned main.ipynb and scanmate.py

--- production/scanmate.py
+++ production/scanmate.py
@@ -340,7 +340,56 @@ def compute_keypoints_descriptors(images: list[Image], SIFT: OpenCV.SIFT, **kwar
 
 
 @timeit
-def image_matching(images_obj: Images, **kwargs):
+def overwriting_similar_images(images: Images, **kwargs) -> dict[str, list[Image]]:
+    image_set_name = kwargs['image_set_name']
+    similar_images_dict: dict[str, dict[str,list[Image]]] = {}
+    if image_set_name == "hammer":
+        similar_images_dict[image_set_name] = {
+                "0": [images[1], images[2], images[3], images[4], images[5]],
+                "1": [images[5], images[6], images[7]],
+                "2": [images[7], images[8], images[9], images[10], images[11], images[12]],
+                "3": [images[12], images[13], images[14], images[15]],
+                "4": [images[15], images[16], images[17], images[18]],
+                "5": [images[18], images[19], images[20]],
+                "6": [images[20], images[21], images[22], images[23]],
+                "7": [images[23], images[24], images[25]],
+                "8": [images[25], images[26], images[27], images[28]],
+                "9": [images[28], images[29], images[30], images[31]],
+                "10": [images[31], images[32], images[33]],
+                "11": [images[33], images[34], images[35]],
+                "12": [images[35], images[36], images[37], images[38]],
+        }
+    elif image_set_name == "cottage":
+        similar_images_dict[image_set_name] = {
+                "0": [images[1], images[2], images[3]],
+                "1": [images[3], images[4], images[5]],
+                "2": [images[5], images[6], images[7]],
+                "3": [images[7], images[8], images[9]],
+                "4": [images[9], images[10], images[11]],
+                "5": [images[11], images[12], images[13]],
+                "6": [images[13], images[14], images[15]],
+                "7": [images[15], images[16], images[17]],
+                "8": [images[17], images[18], images[19]],
+                "9": [images[19], images[20], images[21]],
+                "10": [images[21], images[22], images[23]],
+                "11": [images[23], images[24], images[25]],
+                "12": [images[25], images[26], images[27]],
+                "13": [images[27], images[28], images[29]],
+                "14": [images[29], images[30]]
+        }
+    elif image_set_name == "fountain":
+        similar_images_dict[image_set_name] = {
+                "0": [images[1], images[2], images[3], images[4], images[5]],
+                "1": [images[5], images[6], images[7], images[8]],
+                "2": [images[8], images[9], images[10], images[11]]
+        },
+    return similar_images_dict[image_set_name]
+
+
+@timeit
+def image_matching(images_obj: Images, overwrite:bool =False, **kwargs) -> None:
+    import os
+    import numpy as np
     from sklearn.cluster import KMeans
     from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
     from tensorflow.keras.preprocessing import image as keras_image
@@ -350,24 +399,33 @@ def image_matching(images_obj: Images, **kwargs):
         img = np.expand_dims(img, axis=0)
         img = preprocess_input(img)
         return img
+
     image_set_name = kwargs['image_set_name']
     image_dir = f'data/{image_set_name}/images'
     image_files = os.listdir(image_dir)
     images = [load_image(os.path.join(image_dir, f)) for f in image_files]
     images = np.vstack(images)
+
     import ssl
     import certifi
+
     ssl_context = ssl.create_default_context(cafile=certifi.where())
     ssl._create_default_https_context = ssl._create_unverified_context
     model = ResNet50(weights='imagenet', include_top=False, pooling='avg')
     features = model.predict(images)
+
     kmeans = KMeans(n_clusters=images_obj.num_clusters, random_state=42)
     clusters = kmeans.fit_predict(features)
+
     for i, cluster in enumerate(clusters):
         if cluster not in images_obj.similar_images:
             images_obj.similar_images[cluster] = []
-            images_obj.similar_images[cluster].append(images_obj[int(image_files[i].split(".")[0])])
-    images_obj.similar_images = {key: value for key, value in images_obj.similar_images.items() if len(value) > 1}
+        images_obj.similar_images[cluster].append(images_obj[int(image_files[i].split(".")[0])])
+
+    if overwrite:
+        images_obj.similar_images = overwriting_similar_images()
+    else:
+        images_obj.similar_images = {key: value for key, value in images_obj.similar_images.items() if len(value) > 1}
 
 
 @timeit
@@ -636,7 +694,7 @@ def run(image_set_name: str) -> None:  # sourcery skip: low-code-quality
     NUM_CLUSTERS: Final[int] = 1
     log_to_file(f"data/{image_set_name}/logs/tune.log", f"Running image_set_name {image_set_name} in {mode} mode...")
     images: Optional[Images] = None
-    """ 0. Reload the last state """
+    """ Reloading the last state """
     last_state: str
     if all(
         os.path.isfile(output_file)
@@ -706,49 +764,8 @@ def run(image_set_name: str) -> None:  # sourcery skip: low-code-quality
             log_to_file(f"data/{image_set_name}/logs/tune.log", f"Ref count of images before: {sys.getrefcount(images)}")
             print_size(f"data/{image_set_name}/logs/tune.log", images, "images")
             images.num_clusters = NUM_CLUSTERS
-            image_matching(images, image_set_name=image_set_name)
-            """ FIX """
-            similar_images_dict: dict[str, dict[str, list[Image]]] = {
-                "hammer": {
-                        "0": [images[1], images[2], images[3], images[4], images[5]],
-                        "1": [images[5], images[6], images[7]],
-                        "2": [images[7], images[8], images[9], images[10], images[11], images[12]],
-                        "3": [images[12], images[13], images[14], images[15]],
-                        "4": [images[15], images[16], images[17], images[18]],
-                        "5": [images[18], images[19], images[20]],
-                        "6": [images[20], images[21], images[22], images[23]],
-                        "7": [images[23], images[24], images[25]],
-                        "8": [images[25], images[26], images[27], images[28]],
-                        "9": [images[28], images[29], images[30], images[31]],
-                        "10": [images[31], images[32], images[33]],
-                        "11": [images[33], images[34], images[35]],
-                        "12": [images[35], images[36], images[37], images[38]]
-                },
-                "cottage": {
-                        "0": [images[1], images[2], images[3]],
-                        "1": [images[3], images[4], images[5]],
-                        "2": [images[5], images[6], images[7]],
-                        "3": [images[7], images[8], images[9]],
-                        "4": [images[9], images[10], images[11]],
-                        "5": [images[11], images[12], images[13]],
-                        "6": [images[13], images[14], images[15]],
-                        "7": [images[15], images[16], images[17]],
-                        "8": [images[17], images[18], images[19]],
-                        "9": [images[19], images[20], images[21]],
-                        "10": [images[21], images[22], images[23]],
-                        "11": [images[23], images[24], images[25]],
-                        "12": [images[25], images[26], images[27]],
-                        "13": [images[27], images[28], images[29]],
-                        "14": [images[29], images[30]]
-                },
-                "fountain": {
-                        "0": [images[1], images[2], images[3], images[4], images[5]],
-                        "1": [images[5], images[6], images[7], images[8]],
-                        "2": [images[8], images[9], images[10], images[11]]
-                },
-            }   
-            images.similar_images = similar_images_dict[image_set_name]
-            """ END FIX"""
+            overwrite = True
+            image_matching(images, overwrite=overwrite, image_set_name=image_set_name)
             log_to_file(f"data/{image_set_name}/logs/tune.log", "image matching done")
             images.save_similar_images()
             log_to_file(f"data/{image_set_name}/logs/tune.log", "saved image clusters")
@@ -782,11 +799,11 @@ def run(image_set_name: str) -> None:  # sourcery skip: low-code-quality
             log_to_file(f"data/{image_set_name}/logs/tune.log", f"Ref count of images after: {sys.getrefcount(images)}")
             dump_images_bak(f"data/{image_set_name}/bak/feature-matching-output.pkl", images)
             if mode == Mode.OPTMIZED:
-                if os.path.exists(f"data/{image_set_name}/bak/images-matched.pkl"):
-                    os.remove(f"data/{image_set_name}/bak/images-matched.pkl")
-                    log_to_file(f"data/{image_set_name}/logs/tune.log", f"File data/{image_set_name}/bak/images-matched.pkl removed successfully.")
+                if os.path.exists(f"data/{image_set_name}/bak/matched-images.pkl"):
+                    os.remove(f"data/{image_set_name}/bak/matched-images.pkl")
+                    log_to_file(f"data/{image_set_name}/logs/tune.log", f"File data/{image_set_name}/bak/matched-images.pkl removed successfully.")
                 else:
-                    log_to_file(f"data/{image_set_name}/logs/tune.log", f"File data/{image_set_name}/bak/images-matched.pkl does not exist.")
+                    log_to_file(f"data/{image_set_name}/logs/tune.log", f"File data/{image_set_name}/bak/matched-images.pkl does not exist.")
         log_to_file(f"data/{image_set_name}/logs/tune.log", "Done Feature Matching Step...")
         last_state = "Triangulation Step"
     if last_state == "Triangulation Step":
@@ -832,7 +849,7 @@ def run(image_set_name: str) -> None:  # sourcery skip: low-code-quality
         print_size(f"data/{image_set_name}/logs/tune.log", images, "images")
         log_to_file(f"data/{image_set_name}/logs/tune.log", f"Reference count<images>: {sys.getrefcount(images)}")
         log_to_file(f"data/{image_set_name}/logs/tune.log", gc.collect())
-        # 7. Clustering
+        # 7. Points Clustering
         log_to_file(f"data/{image_set_name}/logs/tune.log", "started clustring....")
         import hdbscan
         start_time = time.time()
@@ -884,11 +901,11 @@ def run(image_set_name: str) -> None:  # sourcery skip: low-code-quality
         combined_mesh = o3d.geometry.TriangleMesh()
         for mesh in camera_meshes + camera_lines:
             combined_mesh += mesh
-        point_cloud_file = f"data/{image_set_name}/output/triangulate/points_cloud.stl"
+        point_cloud_file = f"data/{image_set_name}/output/triangulate/points_cloud.ply"
         o3d.io.write_point_cloud(point_cloud_file, points_cloud_stl)
-        point_cloud_file = f"data/{image_set_name}/output/triangulate/core_points.stl"
+        point_cloud_file = f"data/{image_set_name}/output/triangulate/core_points.ply"
         o3d.io.write_point_cloud(point_cloud_file, core_points_stl)
-        mesh_file = f"data/{image_set_name}/output/triangulate/camera_proj.stl"
+        mesh_file = f"data/{image_set_name}/output/triangulate/camera_proj.ply"
         o3d.io.write_triangle_mesh(mesh_file, combined_mesh)
         # 9. Meshing
         pcd = o3d.geometry.PointCloud()
