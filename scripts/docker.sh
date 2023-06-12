ec439d19bf304cf58720679c15127a8a7c539599
Ziad Mansour <ziadmansour.4.9.2000@gmail.com>
2023-06-12 13:47:18 +0300

    🔧 FIX: added docker install script

---
+++ scripts/docker.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt -y install docker-ce
+# sudo systemctl status docker
+docker --version
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
